f57c479b1b3954bf8509e93ce9066f70704d4e7f
shencang <2630610009@qq.com>
2018-12-07 22:36:49 +0800

    首页可视化

--- app/models/Student.java
+++ app/models/Student.java
@@ -1,23 +1,51 @@
 package models;
 
+import play.data.validation.Email;
+import play.data.validation.MaxSize;
+import play.data.validation.Password;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Student extends Model {
 
+    @Required
+    @MaxSize(11)
     public String stu_id;
+
+    @Required
+    @MaxSize(8)
     public String name;
+
+    @Required
+    @MaxSize(11)
     public String phone_number;
+    @MaxSize(8)
     public String grade;
+    @Required
+    @Password
     public String password;
-    public boolean isAdmin;
+    @Email
     public String email;
+
+    public boolean isAdmin;
+    @Required
+    @ManyToOne
+    public Teacher myTeacher;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
     public Student(String stu_id, String name,
                    String phone_number, String grade,
                    String password, String email) {
+
 
         this.stu_id = stu_id;
         this.name = name;
--- app/models/Teacher.java
+++ app/models/Teacher.java
@@ -1,20 +1,45 @@
 package models;
 
+import play.data.validation.Email;
+import play.data.validation.MaxSize;
+import play.data.validation.Password;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Teacher extends Model {
 
+    @Required
+    @MaxSize(12)
     public String tea_id;
+    @Required
+    @MaxSize(8)
     public String name;
+    @Required
+    @MaxSize(11)
     public String phone_number;
+    @Required
     public String position;
+
+    @Required
+    @Password
     public String password;
-    public boolean isAdmin;
+
+    @Email
     public String email;
 
+    public boolean isAdmin;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
     public Teacher(String tea_id, String name,
                    String phone_number, String position,
@@ -28,4 +53,7 @@ public class Teacher extends Model {
         this.password=password;
         this.email=email;
     }
+
+    @OneToMany(mappedBy = "myTeacher",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    public List<Student>students;
 }
