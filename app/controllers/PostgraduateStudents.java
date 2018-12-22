2bc00d2c106b3c3522cfcdf12317d739be4a127c
shencang <2630610009@qq.com>
2018-12-22 14:13:44 +0800

    本科生，研究生功能暂定

--- app/controllers/PostgraduateStudents.java
+++ app/controllers/PostgraduateStudents.java
@@ -1,4 +1,31 @@
 package controllers;
 
+import models.Student;
+import play.mvc.With;
+
+@With(Secure.class)
+@Check("studentNameModify")
 public class PostgraduateStudents extends CRUD {
+    public static void modifyName(String id){
+        //大改，由二级索引降为一级索引。以学号为准
+        Student object = Student.find("byStu_id", id).first();
+
+        render("@pschangename", object);
+    }
+
+    public static void doModifyName(Student object){
+        Student st = Student.find("byStu_id", object.stu_id).first();
+        //大改，由二级索引降为一级索引。以学号为准
+        if (null != object.name) {
+            // 保存新名字
+            st.name = object.name;
+            st.save();
+            render("@psmodifynamedone", object);
+        }
+    }
+
+    public static void  studentscenter(){
+        render();
+    }
+
 }
--- app/controllers/UndergraduateStudents.java
+++ app/controllers/UndergraduateStudents.java
@@ -1,4 +1,32 @@
 package controllers;
 
+
+import models.Student;
+import play.mvc.With;
+
+@With(Secure.class)
+@Check("studentNameModify")
 public class UndergraduateStudents extends CRUD {
+    public static void modifyName(String id){
+        //大改，由二级索引降为一级索引。以学号为准
+        Student object = Student.find("byStu_id", id).first();
+
+        render("@uschangename", object);
+    }
+
+    public static void doModifyName(Student object){
+        Student st = Student.find("byStu_id", object.stu_id).first();
+        //大改，由二级索引降为一级索引。以学号为准
+        if (null != object.name) {
+            // 保存新名字
+            st.name = object.name;
+            st.save();
+            render("@usmodifynamedone", object);
+        }
+    }
+
+    public static void  studentscenter(){
+        render();
+    }
+
 }
